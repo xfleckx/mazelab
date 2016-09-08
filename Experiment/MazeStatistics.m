09f63b85ddb223234e968987144e04bd45f5e0f0
Markus Fleck <fleckmarkus.privat@gmail.com>
2016-09-08 09:51:54 +0200

    Add Time inCell property and docu

--- Experiment/MazeStatistics.m
+++ Experiment/MazeStatistics.m
@@ -1,7 +1,16 @@
 classdef MazeStatistics
+%% A Container for data about a maze
+%  intendet to be used per trial instance
     properties 
-        MazeModel
+        %reference to the imported model of the maze
+        MazeModel 
         MazeMatrix
+        % A container for a struct providing:
+        % obj.Tics.All = tics; % from one Trial
+        % obj.Tics.AvgTic = mean(tics);
+        % obj.Tics.Std = std(allTics);
+        % it represents the timing informations Time in Cell aka Unit
+        Tics
     end
 
     methods
